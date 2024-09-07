a5b056151b4df44f5a756d5e0e4daef0d3f62d83
yonghwan1106 <sanoramyun8@gmail.com>
2024-09-07 10:20:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import streamlit as st
 from pages import visualization, carbon_calculator, policy_suggestions, eco_game
