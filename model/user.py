d32e5bdc95074ad63e6e4876a5059db03a3c1ae6
yonghwan1106 <sanoramyun8@gmail.com>
2024-09-10 20:57:14 +0900

    Create user.py

---
+++ model/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
