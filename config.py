5be6539e6908d14479f62a6134cd12cb1b64f3be
yonghwan1106 <sanoramyun8@gmail.com>
2024-09-10 22:06:26 +0900

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("1cf39fc713abdabd7385249e18425c05")
