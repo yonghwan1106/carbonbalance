b0ba401ed83a29df92b24eb6eaf0ba1e5da65dce
yonghwan1106 <sanoramyun8@gmail.com>
2024-09-10 22:10:49 +0900

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "sqlite:///carbon_credit.db"
 SECRET_KEY = os.getenv("1cf39fc713abdabd7385249e18425c05")
