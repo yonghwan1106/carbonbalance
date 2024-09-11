ee7f5c23f3a5619d46c61929ed726f69b6c1186a
yonghwan1106 <sanoramyun8@gmail.com>
2024-09-11 11:03:09 +0900

    Update auth_manager.py

--- utils/auth_manager.py
+++ utils/auth_manager.py
@@ -5,6 +5,13 @@ from config import SECRET_KEY
 from models.user import User
 from utils.db_manager import get_db_session
 
+def show_login_page():
+    """
+    Render the login page for unauthenticated users.
+    """
+    # 로그인 페이지 렌더링 로직 구현
+    pass
+
 def authenticate_user(username, password):
     session = get_db_session()
     user = session.query(User).filter_by(username=username).first()
@@ -12,6 +19,8 @@ def authenticate_user(username, password):
     if user and check_password_hash(user.password_hash, password):
         return user
     else:
+        # 인증 실패 시 로그인 페이지 표시
+        show_login_page()
         return None
 
 def create_token(user_id):
@@ -26,6 +35,8 @@ def verify_token(token):
         user_id = payload['user_id']
         return user_id
     except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
+        # 토큰 검증 실패 시 로그인 페이지 표시
+        show_login_page()
         return None
 
 def is_user_authenticated(token):
@@ -38,8 +49,12 @@ def is_user_authenticated(token):
         if user:
             return user
         else:
+            # 사용자 인증 실패 시 로그인 페이지 표시
+            show_login_page()
             return None
     except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
+        # 토큰 검증 실패 시 로그인 페이지 표시
+        show_login_page()
         return None
 
 def login_user(username, password):
@@ -50,12 +65,17 @@ def login_user(username, password):
         token = create_token(user.id)
         return user, token
     else:
+        # 로그인 실패 시 로그인 페이지 표시
+        show_login_page()
         return None, None
 
 def logout_user(token):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
         user_id = payload['user_id']
+        # 로그아웃 처리 구현
         return True
     except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
+        # 토큰 검증 실패 시 로그인 페이지 표시
+        show_login_page()
         return False
