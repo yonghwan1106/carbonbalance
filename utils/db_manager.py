b88aaa35c0e012ee3cfb986d7af5257c5f5f006d
yonghwan1106 <sanoramyun8@gmail.com>
2024-09-11 09:50:25 +0900

    Update db_manager.py

--- utils/db_manager.py
+++ utils/db_manager.py
@@ -1,19 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from config import DATABASE_URL
-from ..models.user import User
-from ..models.credit import Credit
-from ..models.credit import Base as CreditBase
+from models.user import User
+from models.credit import Credit
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
+Base = declarative_base()
 
 def get_db_session():
     return Session()
 
 def init_db():
-    UserBase.metadata.create_all(engine)
-    CreditBase.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
 
 # 애플리케이션 시작 시 이 함수를 호출하세요
 init_db()
