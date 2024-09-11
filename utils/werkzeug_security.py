00d33fb0ae7add90cae1868965a78dbfc918494d
yonghwan1106 <sanoramyun8@gmail.com>
2024-09-11 10:18:05 +0900

    Update and rename werkzeug.security.py to werkzeug_security.py

--- utils/werkzeug_security.py
+++ utils/werkzeug_security.py
@@ -1,4 +1,4 @@
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug_security import generate_password_hash, check_password_hash
 
 def hash_password(password):
     return generate_password_hash(password)
