cae72b098bc24cc9ac34a8e689ab8f26e577bd7e
yonghwan1106 <sanoramyun8@gmail.com>
2024-09-11 10:44:44 +0900

    Update auth_manager.py

--- utils/auth_manager.py
+++ utils/auth_manager.py
@@ -5,6 +5,29 @@ from config import SECRET_KEY
 from models.user import User
 from utils.db_manager import get_db_session
 
+def authenticate_user(username, password):
+    session = get_db_session()
+    user = session.query(User).filter_by(username=username).first()
+    session.close()
+    if user and check_password_hash(user.password_hash, password):
+        return user
+    else:
+        return None
+
+def create_token(user_id):
+    expiration = datetime.utcnow() + timedelta(hours=24)
+    payload = {'user_id': user_id, 'exp': expiration}
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    return token
+
+def verify_token(token):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        user_id = payload['user_id']
+        return user_id
+    except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
+        return None
+
 def is_user_authenticated(token):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
@@ -36,9 +59,3 @@ def logout_user(token):
         return True
     except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
         return False
-
-def create_token(user_id):
-    expiration = datetime.utcnow() + timedelta(hours=24)
-    payload = {'user_id': user_id, 'exp': expiration}
-    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-    return token
