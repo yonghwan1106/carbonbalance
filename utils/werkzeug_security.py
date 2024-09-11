21dca70ac384cff0139512205647e8821d48956c
yonghwan1106 <sanoramyun8@gmail.com>
2024-09-11 10:31:39 +0900

    Delete utils/werkzeug_security.py

--- utils/werkzeug_security.py
+++
@@ -1,7 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-def hash_password(password):
-    return generate_password_hash(password)
-
-def verify_password(password_hash, password):
-    return check_password_hash(password_hash, password)
