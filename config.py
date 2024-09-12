d84b3de2789501a39cfc3deb23a87e10b7e5847d
yonghwan1106 <sanoramyun8@gmail.com>
2024-09-12 09:35:58 +0900

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Sqlite
 DATABASE_URL = "sqlite:///carbon_credit.db"
 SECRET_KEY = os.getenv("1cf39fc713abdabd7385249e18425c05")
+
+
+# Supabase
+SUPABASE_URL = "https://knoluipowadknsjabcqo.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtub2x1aXBvd2Fka25zamFiY3FvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYxMDA0MTksImV4cCI6MjA0MTY3NjQxOX0.LDgUKgjnglG5nNQ_IH4mzmEM89McJafbFrbvhoSETSQ"
