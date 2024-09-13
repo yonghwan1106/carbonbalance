fafa3bd981e2d52cde28eeb162fe35c550d197cd
yonghwan1106 <sanoramyun8@gmail.com>
2024-09-13 09:40:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import streamlit as st
 # 페이지 설정을 스크립트 최상단에 배치
 st.set_page_config(page_title="Carbon neutrality Korea", page_icon="🌿", layout="wide")
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'pages'))
-
 from supabase import create_client, Client
 import hashlib
 from pathlib import Path
